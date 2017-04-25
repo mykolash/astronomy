6c50ec11aa6a62f7872523c604d8484f29c600cf
mykolash <mykolash@gmail.com>
2017-04-25 11:34:44 +0300

    Share by email functional

--- astronomy/settings.py
+++ astronomy/settings.py
@@ -104,3 +104,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'astronomy.mykolash@gmail.com'
+EMAIL_HOST_PASSWORD = 'testuserpassatastronomy'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+
